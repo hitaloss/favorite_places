91cbc65bf41bc90ebc129822787e09740e0f72cd
hitalo <hitalo.2014.hs@gmail.com>
2024-10-18 17:14:50 -0300

    feat: application provider created for places

---
+++ lib/providers/places_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/models/place_model.dart';
+
+class PlacesNotifier extends StateNotifier<List<Place>> {
+  PlacesNotifier() : super([]);
+
+  void addPlace(Place place) {
+    state = [...state, place];
+  }
+}
+
+final placesProvider = StateNotifierProvider<PlacesNotifier, List<Place>>(
+    (ref) => PlacesNotifier());
