65946ea7eaf4b211655eb1965447cd470cb50d7b
hitalo <hitalo.2014.hs@gmail.com>
2024-10-18 17:42:21 -0300

    feat: added providerscope on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:favorite_places/screens/places_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/screens/places_screen.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -26,9 +28,7 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
