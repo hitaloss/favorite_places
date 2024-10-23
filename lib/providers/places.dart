f3c8c0a32a326824ba23e8aca09b42ac29968449
hitalo <hitalo.2014.hs@gmail.com>
2024-10-23 15:43:35 -0300

    fix: added const before the state initialization

--- lib/providers/places.dart
+++ lib/providers/places.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favorite_places/models/place.dart';
 
 class PlacesNotifier extends StateNotifier<List<Place>> {
-  PlacesNotifier() : super([]);
+  PlacesNotifier() : super(const []);
 
   void addPlace(Place place) {
     state = [...state, place];
