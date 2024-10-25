3df28177fd6f221a2e94727bd68e5efe97987d23
hitalo <hitalo.2014.hs@gmail.com>
2024-10-25 12:19:24 -0300

    refactor: removed unecessary Center Widget from file

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -12,10 +12,16 @@ class PlaceDetailsScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: Text(place.title)),
-      body: Center(
-          child: Text(place.title,
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground))),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          )
+        ],
+      ),
     );
   }
 }
