3da2f8f05c8a65fa2cad12b6fbb2919cb92bf533
hitalo <hitalo.2014.hs@gmail.com>
2024-10-29 13:13:31 -0300

    feat: applied map footage on place details screen, along with basic information

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -8,6 +8,13 @@ class PlaceDetailsScreen extends ConsumerWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+
+    return "https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:purple%7Clabel:A%7C$lat,$lng&key=AIzaSyDzTJq7vddGmhQw6lwLwTTkHWI_ogj0CY4";
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
@@ -19,6 +26,36 @@ class PlaceDetailsScreen extends ConsumerWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [Colors.transparent, Colors.black54],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                    child: Text(
+                      place.location.address,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground),
+                    )),
+              ],
+            ),
           )
         ],
       ),
